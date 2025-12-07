c869f48559c9117ed7a5be38d782bd479c48d31e
Arjun Mann <arjunmann.wa@gmail.com>
2025-12-07 15:33:45 -0800

    added encryption lib to auth (4.1)

--- gacha-lc/src/backend/routes/auth.py
+++ gacha-lc/src/backend/routes/auth.py
@@ -2,6 +2,15 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import SessionLocal
 from db import models
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(password: str):
+    return pwd_context.verify(password)
 
 router = APIRouter()
 
