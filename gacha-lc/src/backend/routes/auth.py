31d275cb67b9593fd3c762d99c91b40192068768
Arjun Mann <arjunmann.wa@gmail.com>
2025-12-07 17:35:19 -0800

    register + login routes

--- gacha-lc/src/backend/routes/auth.py
+++ gacha-lc/src/backend/routes/auth.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from db.database import SessionLocal
-from db import models
+from db.models import User
 from passlib.context import CryptContext
+from schemas import UserCreate
+
+from jose import jwt
+from datetime import datetime, timezone, timedelta
+SECRET_KEY = "temp_secret"
+ALGORITHM = "HS256"
+
+def create_access_token(data: str, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=30))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
 
@@ -20,3 +36,23 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+@router.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.username == user.username).first()
+    if existing_user:
+        raise HTTPException(401, "Username already exists")
+    hashed_pw = hash_password(user.password)
+    new_user = User(user.username, hashed_pw)
+    db.add(new_user)
+    db.commit()
+    db.close()
+    return {"message" : "User created successfully"}
+    
+@router.post("/login")
+def login(form: OAuth2PasswordRequestForm, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == form.username).first()
+    if not user or not verify_password(form.password, user.hashed_passowrd):
+        raise HTTPException(401, "Invalid login")
+    token = create_access_token({"sub":str(user.id)})
+    return {"access_token" : token, "token_type": "bearer"}
